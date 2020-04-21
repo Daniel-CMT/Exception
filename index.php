b604a13f09969f3b3b0fcb6e9a33bed59f95caa9
Daniel-CMT <61702198+Daniel-CMT@users.noreply.github.com>
2020-04-21 12:22:58 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once 'cars.php';
+
+$car = new Cars();
+$car->setHasParkBrake(true);
+
+
+try
+{
+
+$car->start();
+}
+catch(Exception $e)
+{
+     $car->setHasParkBrake(false);
+}
+finally
+{
+    echo "Ma voiture roule comme un donut";
+}
+
+
